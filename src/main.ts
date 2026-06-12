c8574e4381c8d666ad73513c4765a87bee654e97
malviyamanish999 <malviyamanish999@gmail.com>
2026-06-12 18:33:37 +0530

    [feature]: console lod adeed

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 3000);
+  console.log('process.env.PORT ', process.env.PORT);
 }
 bootstrap();
